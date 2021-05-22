3bcd91faeb4f8daf4ef9efc69fba911a3d76b2c4
jisethpenarias <1875@holbertonschool.com>
2021-05-22 09:40:27 -0500

    Proyecto inicializado con srping initializr

---
+++ src/test/java/com/grupobolivar/banco/BancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupobolivar.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
